025f712699cbe4a413f9493d756c6181a016a194
golshidjalili <151672432+golshidjalili@users.noreply.github.com>
2023-11-22 19:02:18 +0330

    Add files via upload

---
+++ Sample_Call_Spy.py
@@ -0,0 +1,10 @@
+from Spy import spy
+
+
+# The letter decrypted (in German)
+letter = 'nä qrä sluara däq arvpubxnäiyra. orvz eraynmrä qrdcbpuynäqb qnäxrä qvr uraiötvä eöä fväqböa däq vpu vuärä ndsavpucvt sla qvr taömr tndcsardäqbpunsc, qvr bvr däb trfjuac unorä däq sla qvr evryrä zktyvpuxrvcrä, qnb id brurä, fnb sla qnb föuy qra bpunssräqrä qrdcbpurä trcnä fvaq. fva äruzrä rvärä cvrsrä rväqadpx eöä däbrara arvbr qdapu qrdcbpuynäq zvc däq fraqrä ävr eratrmrä, zvc frypura ndszraxbnzxrvc fva eöä vuarä orndscanträ dztrorä föaqrä bväq, däq rvär fvr uraiyvpur ndsäruzrä fva loranyy trsdäqrä unorä. orböäqrab qnäxrä fva vuärä sla qvr bpukärä bcdäqrä, qvr fva zvc vuärä nds qrz öorabnyiorat eraoanpuc unorä.'
+# Our spy word
+spy_word = 'obersalzberg'
+
+# Ecryption of the letter
+spy(letter, spy_word)
